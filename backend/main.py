d27b551cf4459f6df44bdf16030b422489689c87
Gustavo Henrique <ymooncake@gmail.com>
2024-03-26 13:30:37 -0300

    Working Update

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,26 @@
-import subprocess
 import time
+import subprocess
 
-def listar_processos():
-    comando = "ps aux"
-    processo = subprocess.Popen(comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    saida, erro = processo.communicate()
-    return saida.decode()
+def listagem(): #inserido o comando no shell, aqui do linux
+    command = "ps aux"
+    proccess = subprocess.Popen(command, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, erro = proccess.communicate()
+    return out.decode()
 
-def criar_arquivo(lista_processos, nome_arquivo):
-    with open(nome_arquivo, "w") as arquivo:
-        arquivo.write(lista_processos)
+def arquivo(listaProcessos, arquivoProcessos): #escreve os processos listado em um arquivo
+    with open(arquivoProcessos,"w") as archive:
+        archive.write(listaProcessos)
+    
 
-def main():
+def main():#a cada 30s cria uma nova lista com os processos
     contador = 1
+
     while True:
-        lista_processos = listar_processos()
-        nome_arquivo = f"lista_de_processos_{contador}.txt"
-        criar_arquivo(lista_processos, nome_arquivo)
-        print(f"Arquivo {nome_arquivo} atualizado.")
+        listaProcessos = listagem()
+        arquivoProcessos = f"listinhaDeProcessos_{contador}.txt"
+        arquivo(listaProcessos,arquivoProcessos)
         contador += 1
         time.sleep(30)
 
-if __name__ == "__main__":
+if __name__ =="__main__":
     main()

